b0ab1c45af16fccd3ea7faa1114419c9fc901b57
jemirinjnr <110988149+jemirinjnr@users.noreply.github.com>
2023-09-14 09:49:52 +0100

    First Api call

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+  const [data,setData] = useState(undefined)
+  const getAPIdata = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts/1"
+    let result = await fetch(url);
+    result = await result.json();
+    setData(result);
+  }
+  useEffect(() => {
+    getAPIdata()
+  },)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text style={{ fontSize: 30 }}> API Call</Text>
+      {
+        data?
+        <View>
+          <Text style={{ fontSize: 30 }}> {data.id}</Text>
+          <Text style={{ fontSize: 30 }}> {data.userId}</Text>
+          <Text style={{ fontSize: 30 }}> {data.title}</Text>
+          <Text style={{ fontSize: 30 }}> {data.body}</Text>
+
+        </View>
+        : null
+      }
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
